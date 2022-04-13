5f43133b7383522d9ccf53f7ff4d843a47e25f24
Kristeauf <kristeauf@gmail.com>
2022-04-13 13:27:45 +0200

    forbidden

--- backend/middleware/isOwn.js
+++ backend/middleware/isOwn.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id }).then((sauce) => {
         if (sauce.userId !== req.auth.userId) {
             res.status(403).json({
-                error: new Error("Unauthorized request!"),
+                error: new Error("forbidden request"),
             });
         }
 
